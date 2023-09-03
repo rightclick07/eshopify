acc829b9c5914456ceb7874890fd4f8ed74aad03
ravi <heyravishankar07@gmail.com>
2023-09-03 11:38:29 +0530

    updated angular to 15

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import "./polyfills";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
-bootstrapApplication(AppComponent);
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
