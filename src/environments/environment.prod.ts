9f2ea058a7949dbb14499d3c557e8c01798384a4
Nidhi <heyravishankar07@gmail.com>
2024-05-09 22:15:22 +0530

    configuration changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  productionn: true,
+  production: true,
   baseUrl:"http://97.74.86.95:8080",
   baseAuthLoginUrl:"http://97.74.86.95:8080/api/auth/login",
   baseAuthSignupUrl:"http://97.74.86.95:8080/api/auth/signup",
