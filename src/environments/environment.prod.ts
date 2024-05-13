d8ce460486a1ac9b784ed36e874580d691c02eb7
ravi <heyravishankar07@gmail.com>
2024-05-13 13:55:26 +0530

    updated env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: true,
-  baseUrl:"http://97.74.86.95:8080",
-  baseAuthLoginUrl:"http://97.74.86.95:8080/api/auth/login",
-  baseAuthSignupUrl:"http://97.74.86.95:8080/api/auth/signup",
-  basePaymentPayUrl:"http://97.74.86.95:8080/apis/pg-sandbox/pg/v1/pay"
+  // baseUrl:"http://97.74.86.95:8080",
+  // baseAuthLoginUrl:"http://97.74.86.95:8080/api/auth/login",
+  // baseAuthSignupUrl:"http://97.74.86.95:8080/api/auth/signup",
+  // basePaymentPayUrl:"http://97.74.86.95:8080/apis/pg-sandbox/pg/v1/pay"
+  basePaymentPayUrl: "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
+  baseUrl:"https://api.everse.co.in",
+  baseAuthLoginUrl:"https://api.everse.co.in/api/auth/login",
+  baseAuthSignupUrl:"https://api.everse.co.in/api/auth/signup"
+
 };
